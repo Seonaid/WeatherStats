c8de2f8464dd0d83c2aeed022493f4ac91ef64ac
Seonaid Lee <seonaidl@gmail.com>
2019-08-09 12:13:30 -0300

    initial commit

---
+++ FileProcessing.java
@@ -0,0 +1,18 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+/**
+ * This project reads a number of files of weather data and produces summary and stats
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FileProcessing {
+    public CSVRecord coldestHourInFile(CSVParser parser){
+        CSVRecord coldestHourSoFar = null;
+        return coldestHourSoFar;
+    }
+    
+    public void testColdestHourInFile(){
+        
+    }
+}
